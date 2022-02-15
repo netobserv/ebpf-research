076e9d4dceb2feb7ac35f2a1d22085402b2e8c1f
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2022-02-15 19:00:13 +0530

    Add option for skb loading

--- xflow/xflow_xdp_load.sh
+++ xflow/xflow_xdp_load.sh
@@ -9,13 +9,14 @@ usage()
 }
 
 unload=0
+skb=1
 
-
-while getopts "i:u" opt
+while getopts "i:us" opt
 do
    case "$opt" in
       i ) interface="$OPTARG" ;;
       u ) unload=1 ;;
+      s ) skb=1 ;;
       ? ) usage ;; # Print helpFunction in case parameter is non-existent
    esac
 done
@@ -34,7 +35,19 @@ then
     echo "sudo xdp-loader unload --all $interface"
     sudo xdp-loader unload --all $interface
 else
-    # Load xflow using xdp-loader
-    echo "sudo xdp-loader load $interface -m native src/xflow.o --pin-path /sys/fs/bpf/xflow/$interface"
-    sudo xdp-loader load $interface -m native src/xflow.o --pin-path /sys/fs/bpf/xflow/$interface
+   if [[ $skb -eq 1 ]]
+   then
+      # Load xflow using xdp-loader
+      echo "sudo xdp-loader load $interface -m skb src/xflow.o --pin-path /sys/fs/bpf/xflow/$interface"
+      sudo xdp-loader load $interface -m skb src/xflow.o --pin-path /sys/fs/bpf/xflow/$interface
+   else
+      # Load xflow using xdp-loader
+      echo "sudo xdp-loader load $interface -m native src/xflow.o --pin-path /sys/fs/bpf/xflow/$interface"
+      sudo xdp-loader load $interface -m native src/xflow.o --pin-path /sys/fs/bpf/xflow/$interface
+   fi
 fi
+
+# sudo tc qdisc add dev ens6f0np0 clsact
+# sudo tc filter add dev ens6f0np0 egress bpf da object-file src/xflow_tc.o section xflow
+
+# sudo tc qdisc del dev ens6f0np0 clsact
