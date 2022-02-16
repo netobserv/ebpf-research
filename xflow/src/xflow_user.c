bb1c8abe56935a91fae7647b29a7f99cb4a96eea
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2022-02-16 17:18:00 +0530

    Print metrics from tc interface

--- xflow/src/xflow_user.c
+++ xflow/src/xflow_user.c
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <arpa/inet.h>
+#include <net/if.h>
 
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
@@ -33,6 +34,7 @@
 
 #define MAXLEN 64
 char iface[32];
+unsigned int ifindex; 
 void  print_usage(){
 	printf("./xflow_user -i <interface> \n");
 }
@@ -52,6 +54,7 @@ int parse_params(int argc, char *argv[]) {
       switch (opt) {
 		  case 'i' : 
 		  	strncpy(iface, optarg, 32);
+			ifindex = if_nametoindex(iface);
 		  	break;
 		  default: 
 		  	print_usage(); 
@@ -62,6 +65,9 @@ int parse_params(int argc, char *argv[]) {
 }
 
 char xflow_pin_base_dir[MAXLEN] =  "/sys/fs/bpf/xflow/";
+char xflow_tc_pin_base_dir[MAXLEN] =  "/sys/fs/bpf/tc/globals/";
+char xdp_map_name[] = "xflow_metric_map";
+char tc_map_name[] = "xflow_metric_tc_map";
 
 void get_ip_string(__u32 ip, char *ip_string) {
     unsigned char bytes[4];
@@ -76,11 +82,12 @@ void get_ip_string(__u32 ip, char *ip_string) {
 int main(int argc, char **argv)
 {
 	
-	int xflow_metric_map_fd;
+	int xflow_xdp_metric_map_fd;
+	int xflow_tc_metric_map_fd;
+
 	flow_id flow_key = {};
 	flow_id next_flow_key;
 	flow_counters my_flow_counters;
-	char map_name[] = "xflow_metric_map";
 	char saddr_string[32];
 	char daddr_string[32];
 
@@ -91,21 +98,22 @@ int main(int argc, char **argv)
 
 	strcat(xflow_pin_base_dir, iface);
 
-	/* Open the map for geneve config */
-	xflow_metric_map_fd = open_bpf_map_file(xflow_pin_base_dir, map_name, NULL);
-	if (xflow_metric_map_fd < 0) {
+	/* Open the map for xdp metrics */
+	xflow_xdp_metric_map_fd = open_bpf_map_file(xflow_pin_base_dir, xdp_map_name, NULL);
+	if (xflow_xdp_metric_map_fd < 0) {
 	  	fprintf(stderr,"ERR: opening map\n");
 		return EXIT_FAIL_BPF;
 	}
 
-	printf("map dir: %s \n", xflow_pin_base_dir);
+	printf("map file: %s/%s \n", xflow_pin_base_dir, xdp_map_name);
+	printf("map file: %s/%s \n", xflow_tc_pin_base_dir, tc_map_name);
 
 	/* Get the flow_maps iteratively using bpf_map_get_next_key() */
 	/* TODO: Convert it into a known format */
-	printf("####### Flow Counters for interface %s #######\n", iface);
+	printf("####### Incoming Flow Counters for interface %s(%d) #######\n", iface, ifindex);
 	printf("Flow Start time  |  Flow End Time   | Src IP Addr:Port  | Dst IP Addr:Port  |   Packets  |  Bytes     |\n");
-	while (bpf_map_get_next_key(xflow_metric_map_fd, &flow_key, &next_flow_key) == 0) {
-		bpf_map_lookup_elem(xflow_metric_map_fd, &next_flow_key, &my_flow_counters);
+	while (bpf_map_get_next_key(xflow_xdp_metric_map_fd, &flow_key, &next_flow_key) == 0) {
+		bpf_map_lookup_elem(xflow_xdp_metric_map_fd, &next_flow_key, &my_flow_counters);
 		get_ip_string(next_flow_key.saddr, saddr_string);
 		get_ip_string(next_flow_key.daddr, daddr_string);
 		//printf("**** Entry : %d ****\n", entry);
@@ -118,18 +126,38 @@ int main(int argc, char **argv)
 			ntohs(next_flow_key.dport),
 			my_flow_counters.packets,
 			my_flow_counters.bytes);
-		// printf("Flow-id : src-ip %s -> dest-ip %s, proto:%d, sport:%d, dport:%d \n", 
-		// 	saddr_string, 
-		// 	daddr_string, 
-		// 	next_flow_key.protocol,
-		// 	next_flow_key.sport,
-		// 	next_flow_key.dport);
-		// //printf("Counters : packets:%d ; bytes:%lld b\n", 
-		// 	my_flow_counters.packets,
-		// 	my_flow_counters.bytes);
+		flow_key = next_flow_key;
+	}
+
+	/* Open the map for xdp metrics */
+	xflow_tc_metric_map_fd = open_bpf_map_file(xflow_tc_pin_base_dir, tc_map_name, NULL);
+	if (xflow_tc_metric_map_fd < 0) {
+	  	fprintf(stderr,"ERR: opening map\n");
+		return EXIT_FAIL_BPF;
+	}
+
+	/* Get the flow_maps iteratively using bpf_map_get_next_key() */
+	/* TODO: Convert it into a known format */
+	printf("####### Outgoing Flow Counters for interface %s #######\n", iface);
+	printf("Flow Start time  |  Flow End Time   | Src IP Addr:Port  | Dst IP Addr:Port  |   Packets  |  Bytes     |\n");
+	while (bpf_map_get_next_key(xflow_tc_metric_map_fd, &flow_key, &next_flow_key) == 0) {
+		bpf_map_lookup_elem(xflow_tc_metric_map_fd, &next_flow_key, &my_flow_counters);
+		if (next_flow_key.interface == ifindex) {
+			get_ip_string(next_flow_key.saddr, saddr_string);
+			get_ip_string(next_flow_key.daddr, daddr_string);
+			//printf("**** Entry : %d ****\n", entry);
+			printf("%llu | %llu | %s:%d | %s:%d | %d | %lld\n", 
+				my_flow_counters.flow_start_ns,
+				my_flow_counters.flow_end_ns,
+				saddr_string,
+				ntohs(next_flow_key.sport),
+				daddr_string,
+				ntohs(next_flow_key.dport),
+				my_flow_counters.packets,
+				my_flow_counters.bytes);
+		}
 		flow_key = next_flow_key;
 	}
 	printf("##############\n");
-
 	return EXIT_OK;
 }
